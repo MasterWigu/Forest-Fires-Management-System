71d9add3c01b757de8e222f1151d0dd78c5d674d
Wigu <moreira.oliveira@live.com.pt>
2018-11-28 11:55:28 +0000

    rip esqueci-me do resto

--- Queries/d_request_b.php
+++
@@ -1,9 +0,0 @@
-<html>
-    <body>
-        <form action="d)a.php" method="post">
-            <p>What's the Number of Process: <input type="text" name="numprocessosocorro"/></p>
-            <p>What's the number of the Vehicle: <input type="text" name="eventoemergencia"/></p>
-            <p><input type="submit" value="Submit"/></p>
-        </form>
-    </body>
-</html>
