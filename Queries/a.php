087e9f66a1776fda925250cc0fef67d44f0ceb9c
Unknown <daniela.alexandram@gmail.com>
2018-11-28 11:51:03 +0000

    a)

--- Queries/a.php
+++ Queries/a.php
@@ -2,10 +2,13 @@
     <meta charset="UTF-8">
 	<body>
 <?php
+
+	$opcao = $_REQUEST['opcao'];
+	$moradalocal = $_REQUEST['moradalocal'];
+	$nomeentidade = $_REQUEST['nomeentidade'];
+
 	try
 	{
-		$moradalocal = $_REQUEST['moradalocal'];
-
 
 		$host = "db.ist.utl.pt";
 		$user ="ist187689";
@@ -15,13 +18,33 @@
 		$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
 		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$sql = "INSERT INTO localidade VALUES (:moradalocal)";
 
-		$result = $db->prepare($sql);
+		if ($opcao == 1) {
+		
+			$sql = "INSERT INTO localidade VALUES (:moradalocal)";
 
-		$result->execute([':moradalocal' => $moradalocal]);
+			$result = $db->prepare($sql);
 
-		echo('Adicionado');
+			$result->execute([':moradalocal' => $moradalocal]);
+
+			echo("Morada adicionada");
+		}
+
+		elseif ($opcao == 9) {
+
+			$sql = "INSERT INTO entidademeio VALUES (:nomeentidade)";
+
+			$result = $db->prepare($sql);
+
+			$result->execute([':nomeentidade' => $nomeentidade]);
+
+			echo("Entidade adicionada");
+		}
+
+		else {
+
+		}
+
 		$db = null;
 	}
 	catch (PDOException $e)
--- Queries/a_request.php
+++ Queries/a_request.php
@@ -3,20 +3,12 @@
 <?php
     
 	$option = $_REQUEST['option'];
-		/*
-	$host = "db.ist.utl.pt";
-    $user ="ist187689";
-    $password = "amarelo23";
-    $dbname = $user;
-    
-    $db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-*/
+
     if ($option == 1) {
         echo("<form action=\"a.php\" method=\"post\">");
     	echo("<p>What's the new address? <input type=\"text\" name=\"moradalocal\"/></p>");
     	echo("<p><input type=\"submit\" value=\"Submit\"/></p>");
-    	echo("<a href=\"a.php?option=1\"></a></form>\n");
+    	echo("<a href=\"a.php?opcao=1\"></a></form>\n");
     }
 
     elseif ($option == 2) {
@@ -48,14 +40,15 @@
     }
 
     elseif ($option == 9) {
-    	
+    	echo("<form action=\"a.php\" method=\"post\">");
+        echo("<p>What's the new entity's name? <input type=\"text\" name=\"nomeentidade\"/></p>");
+        echo("<p><input type=\"submit\" value=\"Submit\"/></p>");
+        echo("<a href=\"a.php?opcao=9\"></a></form>\n");
     }
 
     else {
-    	
-    }
 
-    #$db = null;
+    }
 
 
 ?>
