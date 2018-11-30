2f66dfc4789a2a41c18202229a7613fc530acefc
Wigu <moreira.oliveira@live.com.pt>
2018-11-30 21:44:37 +0000

    cleanup

--- web/e)request.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		<form action="../Queries/e).php" method="post">
-			<p>Qual o numero do processo? <input type="text" name="numprocessosocorro"/></p>
-			<p><input type="submit" value="Submit"/></p>
-		</form>
-	</body>
-</html>
--- web/f)request.php
+++
@@ -1,8 +0,0 @@
-<html>
-	<body>
-		<form action="../Queries/f).php" method="post">
-			<p> Qual e a localizacao do incendio? <input type="text" name="moradalocal"/></p>
-			<p><input type="submit" value="Submit"/></p>
-		</form>
-	</body>
-</html>
