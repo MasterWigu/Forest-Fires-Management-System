3648eac3d6d70d7a3d24033dc87ef7a08815936e
Miguel Oliveira <moreira.oliveira@live.com.pt>
2018-12-12 13:14:23 +0000

    skskskksksksksksks

--- Dados/schema_new.sql
+++ Dados/schema_new.sql
@@ -174,3 +174,49 @@ create table solicita(
     constraint fk_solicita_video foreign key(dataHoraInicioVideo, numCamara) references video(dataHoraInicio, numCamara) on delete cascade,
     check (datahoraInicio < datahoraFim)
 );
+
+
+/* a */
+create or replace function update_solicita()
+    returns trigger as $$
+    begin
+        if not exists (
+            select moradaLocal
+            from audita natural join eventoEmergencia natural join vigia
+            where idCoordenador = new.idCoordenador
+                and numCamara = new.numCamara
+        ) then
+            raise exception 'Merda1';
+        end if;
+        return new;
+    end;
+$$ language plpgsql;
+
+create trigger update_solicita_trigger before insert on solicita
+  for each row execute procedure update_solicita();
+
+
+/* b */
+
+/*imprimir a dizer que não pode ser alocado; imprimir a dizer que foi alocado */
+create or replace function update_alocado()
+    returns trigger as $$
+    begin
+        if not exists (
+            select numMeio
+            from acciona
+            where acciona.numMeio = new.numMeio
+            and acciona.nomeEntidade = new.nomeEntidade
+            and acciona.numProcessoSocorro = new.numProcessoSocorro
+        ) then
+            raise exception 'Merda2';
+        end if;
+        return new;
+    end;
+$$ language plpgsql;
+
+create trigger update_alocado_trigger before insert on alocado
+  for each row execute procedure update_alocado();
+
+
+
