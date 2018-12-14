d85c636b20f70a72968ead746cb11ea567590c3f
Unknown <daniela.alexandram@gmail.com>
2018-12-14 08:36:18 +0000

    data analytics tinha um erro

--- Dados/data_analytics.sql
+++ Dados/data_analytics.sql
@@ -1,23 +1,23 @@
-select tipo, ano, mes, sum(idMeio)
+select tipo, ano, mes, count(idMeio)
 from d_junta, d_meio
 where idEvento = 15
 group by rollup(tipo, ano, mes);
 
-select tipo, ano, mes, sum(idMeio)
+select tipo, ano, mes, count(idMeio)
 from d_junta, d_meio
 where idEvento = 15
 group by tipo, ano, mes
 union
-select tipo, ano, null, sum(idMeio)
+select tipo, ano, null, count(idMeio)
 from d_junta, d_meio
 where idEvento = 15
 group by tipo, ano
 union
-select tipo, null, null, sum(idMeio)
+select tipo, null, null, count(idMeio)
 from d_junta, d_meio
 where idEvento = 15
 group by tipo
 union
-select null, null, null, sum(idMeio)
+select null, null, null, count(idMeio)
 from d_junta, d_meio
 where idEvento = 15;
